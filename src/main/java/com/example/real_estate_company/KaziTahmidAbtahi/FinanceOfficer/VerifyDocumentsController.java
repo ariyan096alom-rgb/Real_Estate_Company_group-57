3394b882ce76bdd8bbb2dd2c434bad428ae06a32
tahmidabtahi2022 <tahmid.abtahi2022@gmail.com>
2026-04-08 10:54:57 +0600

    Some File Issue Fixed

---
+++ src/main/java/com/example/real_estate_company/KaziTahmidAbtahi/FinanceOfficer/VerifyDocumentsController.java
@@ -0,0 +1,38 @@
+package com.example.real_estate_company.KaziTahmidAbtahi.FinanceOfficer;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class VerifyDocumentsController
+{
+    @javafx.fxml.FXML
+    private Label feedbackLabel;
+    @javafx.fxml.FXML
+    private TextField clientIncomeTF;
+    @javafx.fxml.FXML
+    private TextArea notesTextArea;
+    @javafx.fxml.FXML
+    private TextField clientNidTF;
+    @javafx.fxml.FXML
+    private ComboBox incomeVerifiedCB;
+    @javafx.fxml.FXML
+    private ComboBox nidVerifiedCB;
+    @javafx.fxml.FXML
+    private ComboBox bookingSelectionCB;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @Deprecated
+    public void recordPaymentButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void loadDocumentDataOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void submitVerificationButtonOnAction(ActionEvent actionEvent) {
+    }
+}
